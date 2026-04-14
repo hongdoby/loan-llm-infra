59b2a17cba3dd461203cb5c05688ddd88f19f006
chikachik4 <wwwonwod0@gmail.com>
2026-04-14 13:15:17 +0900

    fix: EKS 모듈 참조 오류 및 IAM 역할 중복 정의 해결
    - 루트 main.tf에 누락되었던 eks_cluster 모듈 호출 추가
    - 모듈 간 직접 참조 오류 해결을 위해 eks-cluster 모듈에 subnet_ids 변수 도입
    - 루트 main.tf에서 eks_cluster 모듈로 프라이빗 서브넷 ID 배열 전달
    - modules/eks-cluster/main.tf와 iam.tf 간의 IAM 역할(eks_cluster_role) 중복 생성 오류 해결을 위해 main.tf 내 중복 코드 제거

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,3 +10,15 @@ module "network" {
   private_subnet_2a_cidr = var.private_subnet_2a_cidr
   private_subnet_2c_cidr = var.private_subnet_2c_cidr
 }
+
+module "eks_cluster" {
+  source = "./modules/eks-cluster"
+
+  cluster_name = var.cluster_name
+  
+  # network 모듈에서 생성된 프라이빗 서브넷 ID를 배열 형태로 전달
+  subnet_ids   = [
+    module.network.private_subnet_2a_id,
+    module.network.private_subnet_2c_id,
+   ]
+}
